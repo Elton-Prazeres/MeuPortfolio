b480ac4f36df8f230d53efb9aeb970b536110884
Elton-Prazeres <elton_hime20@hotmail.com>
2025-03-17 20:59:40 -0300

    linhas 8,9 inserido ; , linha 17 const navMenu(header, .navegacapo-primaria), navMenu toggle ativado

--- js/placard.js
+++ js/placard.js
@@ -5,8 +5,8 @@ function escrevendoLetra(){
         arrTexto.forEach((letra, i)=>{
             setTimeout(()=>{
                 elemento.innerHTML += letra;
-            }, 75 * i)
-        })
+            }, 75 * i);
+        });
     }
 
     const titulo = document.querySelector('.digitando');
@@ -14,7 +14,9 @@ function escrevendoLetra(){
 }
 
 const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
 
 ativaMenu.addEventListener('click', ()=>{
     ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
 })
